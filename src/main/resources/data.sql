ea9574d55f28164db9e6f7559c1c6ec45c086819
Mathangi Pushparajah <mathangi23@outlook.com>
2024-06-27 13:55:19 +0200

    add dummy data into scoreboard table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,12 @@ VALUES (DEFAULT, 1),
        (DEFAULT, 'fizz'),
        (DEFAULT, 4),
        (DEFAULT, 'buzz');
+
+
+INSERT INTO scoreboard (user_name, time, score)
+VALUES ('Lucy', '2020-08-01T12:15:30', 15),
+       ('Math', '2019-01-01T05:15:30', 15),
+       ('Khanrieh', '2022-02-01T11:15:30', 15),
+       ('Hannah', '2004-03-05T11:15:30', 15),
+       ('Trell', '2024-04-10T09:15:30', 15),
+       ('Pon', '2012-05-01T12:15:30', 15);

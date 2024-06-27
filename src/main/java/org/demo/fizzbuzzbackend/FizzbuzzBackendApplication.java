15ab8b37f958bebde217edc6c9472b33ddc3f665
Mathangi Pushparajah <mathangi23@outlook.com>
2024-06-27 08:46:49 +0200

    Initial commit

---
+++ src/main/java/org/demo/fizzbuzzbackend/FizzbuzzBackendApplication.java
@@ -0,0 +1,13 @@
+package org.demo.fizzbuzzbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzbuzzBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FizzbuzzBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/demo/fizzbuzzbackend/FizzbuzzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.fizzbuzzbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzbuzzBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

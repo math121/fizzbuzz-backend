9a9e70b866f195e23cde65e2a14ad161e18e258e
Mathangi Pushparajah <mathangi23@outlook.com>
2024-06-28 16:31:10 +0200

    implement getting and adding scores

--- src/main/java/org/demo/fizzbuzzbackend/services/ScoreboardService.java
+++ src/main/java/org/demo/fizzbuzzbackend/services/ScoreboardService.java
@@ -1,6 +1,8 @@
 package org.demo.fizzbuzzbackend.services;
 
+import org.demo.fizzbuzzbackend.models.FizzBuzzUser;
 import org.demo.fizzbuzzbackend.models.Scoreboard;
+import org.demo.fizzbuzzbackend.repos.FizzBuzzUserRepository;
 import org.demo.fizzbuzzbackend.repos.ScoreboardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,17 +15,39 @@ public class ScoreboardService {
     @Autowired
     private ScoreboardRepository repository;
 
+    @Autowired
+    private FizzBuzzUserRepository userRepository;
+
     public List<Scoreboard> getScoreboard() {
-        return repository.getAllSorted();
+        return repository.findAllByOrderByScoreDesc();
     }
 
-    public Scoreboard addScoreboard(Scoreboard scoreboard) {
+    public Scoreboard addScoreboard(String userName, Scoreboard scoreboard) {
+        FizzBuzzUser existingUser = userRepository.findByUserName(userName);
+        if (existingUser != null) {
+            Scoreboard existingScoreboard = repository.findScoreboardByFizzBuzzUser(existingUser);
+            scoreboard.setId(existingScoreboard.getId());
+            scoreboard.setFizzBuzzUser(existingUser);
+            return repository.save(scoreboard);
+        }
+
+        FizzBuzzUser newUser = userRepository.save(new FizzBuzzUser(userName, "Not given"));
+        scoreboard.setFizzBuzzUser(newUser);
+
         return repository.save(scoreboard);
     }
 
-    public int updateNameOnScoreboard(Long id, String name) {
-        return repository.updateNameOnScoreboard(id, name);
+    public Scoreboard getScoreboardForUser(String name) {
+        FizzBuzzUser existingUser = userRepository.findByUserName(name);
+        if (existingUser != null) {
+            return repository.findScoreboardByFizzBuzzUser(existingUser);
+        }
+        return null;
     }
+
+    /*public int updateNameOnScoreboard(Long id, String name) {
+        return repository.updateNameOnScoreboard(id, name);
+    }*/
 
     public void deleteScoreboardById(Long id) {
         repository.deleteById(id);

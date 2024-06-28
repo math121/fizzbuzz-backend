74ec2444fb873c43097c9e566759de40b56f8062
Mathangi Pushparajah <mathangi23@outlook.com>
2024-06-28 16:25:39 +0200

    update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,11 +8,20 @@ VALUES (DEFAULT, 1),
        (DEFAULT, 7),
        (DEFAULT, 8);
 
+INSERT INTO fizz_buzz_user (user_name, country)
+VALUES ('Lucy', 'Finland'),
+       ('Math', 'Sweden'),
+       ('Khanrieh', 'Norway'),
+       ('Hannah', 'Denmark'),
+       ('Trell', 'Germany'),
+       ('Pon', 'Sweden');
 
-INSERT INTO scoreboard (id, user_name, time, score)
-VALUES (DEFAULT, 'Lucy', '2020-08-01T12:15:30', 42),
-       (DEFAULT, 'Math', '2019-01-01T05:15:30', 10),
-       (DEFAULT, 'Khanrieh', '2022-02-01T11:15:30', 55),
-       (DEFAULT, 'Hannah', '2004-03-05T11:15:30', 25),
-       (DEFAULT, 'Trell', '2024-04-10T09:15:30', 1),
-       (DEFAULT, 'Pon', '2012-05-01T12:15:30', 5);
+INSERT INTO scoreboard (time, score, user_id)
+VALUES ('2020-08-01T12:15:30', 42, 1),
+       ('2019-01-01T05:15:30', 10, 2),
+       ('2022-02-01T11:15:30', 55, 3),
+       ('2004-03-05T11:15:30', 25, 4),
+       ('2024-04-10T09:15:30', 1, 5),
+       ('2024-04-10T09:15:30', 7, 6);
+
+
